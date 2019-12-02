806cfb97809c4e478872ad1daf57bf8819364388
UFo_x <tero.jaakkola1@gmail.com>
2019-12-02 12:52:46 +0200

    feat(mock): create mock based on figma draft

--- www/components/Navigation.js
+++ www/components/Navigation.js
@@ -4,15 +4,6 @@ const Navigation = () => {
   return (
     <nav className="flex items-center justify-between flex-wrap bg-white py-2 px-4">
       <div className="flex items-center flex-shrink-0 text-black mr-6">
-        <svg
-          className="fill-current h-8 w-8 mr-2"
-          width="54"
-          height="54"
-          viewBox="0 0 54 54"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-        </svg>
         <span className="font-semibold text-xl tracking-tight">
           Pizza italia
         </span>
@@ -20,12 +11,12 @@ const Navigation = () => {
       <div className="block">
         <button className="flex items-center px-3 py-2 text-black hover:text-gray-700">
           <svg
-            className="fill-current h-3 w-3"
-            viewBox="0 0 20 20"
+            className="fill-current text-black hover:text-gray-700 h-6 w-6"
+            viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
           >
             <title>Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            <path d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z" />
           </svg>
         </button>
       </div>
