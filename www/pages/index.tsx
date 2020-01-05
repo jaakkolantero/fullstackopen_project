037a18a51c4af2eb005d6119f79ca8eca37102e1
UFo_x <tero.jaakkola1@gmail.com>
2020-01-05 09:43:37 +0200

    refactor(config): make graphql api url env variable

--- www/pages/index.tsx
+++ www/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import useSWR from "swr";
 import { request } from "graphql-request";
 
@@ -8,15 +8,16 @@ import Menu from "../components/Menu";
 import Cart from "../components/Cart";
 import OrderDetails from "../components/OrderDetails";
 
-const API = "http://localhost:3000/api/graphql";
+const API = process.env.GRAPHQL_API;
 
 const Index = () => {
   const query = `{menuItems{id,name,ingredients,price}}`;
   const { data, error } = useSWR(query, query => request(API, query));
+  const [cart, setCart] = useState();
 
   if (error) return <div>failed to load</div>;
   if (!data) return <div>loading...</div>;
-  if (data) console.log("data", data);
+  console.log("data", data);
   return (
     <>
       <Navigation />
