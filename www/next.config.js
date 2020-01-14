60aedd09d9d936c92d4d294449149cfdaca64f7b
UFo_x <tero.jaakkola1@gmail.com>
2020-01-14 13:25:27 +0200

    add  netlifycms.

--- www/next.config.js
+++ www/next.config.js
@@ -1,6 +1,14 @@
 const withCSS = require("@zeit/next-css");
 
 module.exports = withCSS({
+  webpack: cfg => {
+    cfg.module.rules.push({
+      test: /\.md$/,
+      loader: "frontmatter-markdown-loader",
+      options: { mode: ["react-component"] },
+    });
+    return cfg;
+  },
   target: "serverless",
   env: {
     GRAPHQL_API: "http://localhost:3000/api/graphql",
