bbb064fa69271a33a609bc5d48590f771d99fc76
UFo_x <tero.jaakkola1@gmail.com>
2020-01-20 21:32:26 +0200

    feat(tina-git-api): add Tina git api. works when running on localhost

--- www/pages/_app.tsx
+++ www/pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import App from "next/app";
 import Head from "next/head";
+import { Tina, TinaCMS } from "tinacms";
 import "../styles/index.css";
 
 export interface LayoutProps {
@@ -20,12 +21,20 @@ const Layout = ({ children }: LayoutProps) => {
 };
 
 export default class MyApp extends App {
+  cms: TinaCMS;
+  constructor(props) {
+    super(props);
+    // initialize the cms
+    this.cms = new TinaCMS();
+  }
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <Tina position="fixed" cms={this.cms}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Tina>
     );
   }
 }
